6aadde0bef44917ca0ba734faa909dd8f2045c0a
Codedys <oberegeraldjackson@gmail.com>
2024-09-03 10:53:35 +0300

    broken out of the server sending loop

--- Clientsocket.py
+++ Clientsocket.py
@@ -4,36 +4,30 @@ from player import Player
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server_address = (socket.gethostname(),10000)
-
 sock.connect(server_address)
-
 
 print("Enter START to start music player")
 print ("Enter STOP to stop music player")
+print(server_address)
 start_or_stop = input("")
 
-try:
-        
+try:      
         if start_or_stop == "START":
             sock.sendall(start_or_stop.encode('utf-8'))
-        
         with open("sing.mp3",'wb') as f:
-            song = sock.recv(1024)
-            while song:
-                f.write(song)
+            while True:
                 song = sock.recv(1024)
-                
-        
-        print("Song copied")
-        
+                if not song:
+                     print("done")
+                     break
+                f.write(song)
+                print("writing")  
+        print("Song copied") 
         game = Player("sing.mp3")
-
         while True:
             print("Enter S to start song P to pause R to resume and ST to stop")
             cstart = input("")
-
             if cstart == "S":
                 game.play()
             elif cstart == "P":
@@ -43,8 +37,6 @@ try:
             elif cstart == "ST":
                 game.stop()
                 break
-
-
 finally:
         sock.close()
     
--- serversocket.py
+++ serversocket.py
@@ -5,47 +5,32 @@ import socket
 #bind socket to host
 #listen for incoming connections
 #Accept incoming connections
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server_address = (socket.gethostname(),10000)
-
 sock.bind(server_address)
-
 sock.listen(1)
-
-path = "/home/gerald/Downloads/Music/humble.mp3"
+path = "/home/jackson/Downloads/Anguka.mp3"
 
 # songs = Library(path)
-
 while True:
     connection,client_address = sock.accept()
-
     try:
-       
-       
-
         while True:
-
             data_input = connection.recv(1024).decode('utf-8')
             data_output = None
-
             if data_input == "START":
                 # file = songs.next_song()
                 print("connection to  ",client_address," sucess")
-
                 with open (path,'rb') as f:
-                    data_output = f.read(1024)
-                    while data_output:
-                        connection.sendall(data_output)
+                    while True:
                         data_output = f.read(1024)
-                    
-
-                
-               
+                        if not data_output:
+                            print("Done")
+                            break
+                        connection.sendall(data_output)
+                        print("sending.....................")
             elif data_input == "STOP":
-                break
-                   
+                break               
     finally:
         connection.close()
 
